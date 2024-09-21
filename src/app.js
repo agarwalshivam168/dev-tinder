62560b341b1de3b77ee6b095ccabc22a594c9e62
agarwal168 <agarwalshivam168@gmail.com>
2024-09-21 15:35:42 +0530

    exploring HTTP methods

--- src/app.js
+++ src/app.js
@@ -4,9 +4,21 @@ const express = require("express");
 
 const app = express();
 
-app.use('/', (req, res) => {
-    res.send('This is homepage!')
+app.get('/user', (req, res) => {
+    res.send({"firstName": "Shivam", "lastName": "Agarwal"})
 })
+
+app.post('/user', (req, res) => {
+    res.send('This is Dashboard!')
+})
+
+app.use('/dashboard', (req, res) => {
+    res.send('This is Dashboard!')
+})
+
+// app.use('/', (req, res) => {
+//     res.send('This is homepage!')
+// })
 
 const PORT = "7777";
 app.listen(PORT, () => {
